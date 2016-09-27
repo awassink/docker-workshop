0694120178f8919efda3ffa00cdbe564da2f4240
Arjen Wassink <awassink@quintor.nl>
2016-09-27 17:19:20 +0200

    added reverse proxy conf

--- three-tier-app/frontend/components/album/services.js
+++ three-tier-app/frontend/components/album/services.js
@@ -4,7 +4,7 @@
     angular.module("cddb4client").factory("albumService", ["$resource", AlbumService]);    
            
     function AlbumService($resource){
-        return $resource("http://10.150.4.198:8081/cddb/rest/:id", { id: "@id"},
+        return $resource("/cddb/rest/:id", { id: "@id"},
             {
                 'update': { method: 'put'}
             }, 
@@ -14,7 +14,7 @@
     angular.module("cddb4client").factory("trackService", ["$resource", TrackService]);    
            
     function TrackService($resource){
-        return $resource("http://10.150.4.198:8081/cddb/rest/:albumid/tracks/:trackid",
+        return $resource("/cddb/rest/:albumid/tracks/:trackid",
             {
                 albumid: "@albumid",
                 trackid: "@trackid"
